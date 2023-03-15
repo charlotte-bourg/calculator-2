f5809c0387462f538831a65f74a0d9b6427b3c1a
Charlotte <charlotte.bourg@gmail.com>
2023-03-15 00:11:41 -0700

    some input validation added

--- calculator.py
+++ calculator.py
@@ -13,23 +13,36 @@ while exit_condition_not_reached:
     else:
         token = expression.split(' ')
         function_name = token[0]
-        num1 = float(token[1])
-        if len(token) == 3: 
-            num2 = float(token[2])
-        if function_name == "+":
-            print(add(num1, num2))
-        if function_name == "-":
-            print(subtract(num1, num2))
-        if function_name == "*":
-            print(multiply(num1, num2))
-        if function_name == "/":
-            print(divide(num1, num2))
+        try:
+            num1 = float(token[1])
+        except ValueError:
+            print("Please enter numerical value.")
+            continue
         if function_name == "square":
             print(square(num1))
-        if function_name == "cube":
+        elif function_name == "cube":
             print(cube(num1))
-        if function_name == "pow":
-            print(power(num1, num2))
-        if function_name == "mod":
-            print(mod(num1, num2))
+        else: 
+            if len(token) < 3: 
+                print("Invalid input: please enter the name of the operator followed by the correct number of values")
+            else:
+                try:
+                    num2 = float(token[2])
+                except ValueError:
+                    print("Please enter numerical value.")
+                    continue
+                if function_name == "+":
+                    print(add(num1, num2))
+                elif function_name == "-":
+                    print(subtract(num1, num2))
+                elif function_name == "*":
+                    print(multiply(num1, num2))
+                elif function_name == "/":
+                    print(divide(num1, num2))
+                elif function_name == "pow":
+                    print(power(num1, num2))
+                elif function_name == "mod":
+                    print(mod(num1, num2))
+                else:
+                    print("Enter one of the following operations: +, -, *, /, square, cube, pow, mod")
 
