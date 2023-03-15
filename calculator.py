63296dae8ca4bc60222cfd4b861bf6d45dfe56c4
Charlotte <charlotte.bourg@gmail.com>
2023-03-14 23:13:50 -0700

    implemented addition function

--- calculator.py
+++ calculator.py
@@ -3,5 +3,16 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+exit_condition_not_reached = True
+while exit_condition_not_reached:
+    expression = input("> ")
+    if expression == 'q':
+        print("Exiting calculator")
+        exit_condition_not_reached = False
+        break
+    else:
+        token = expression.split(' ')
+        if token[0] == "+":
+            print(add(float(token[1]), float(token[2])))
+    
 
-# Replace this with your code
